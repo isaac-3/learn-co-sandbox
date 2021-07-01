0fde6ae5b2767ffea30383d25c7587bc02c81373
isaac chavez <isaacgc0596@gmail.com>
2021-07-01 21:41:30 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,17 @@
+
+# puts "Hello World"
+# puts "hi"
+
+# counter = 0
+# until counter == 20
+#   puts "The current number is less than 20."
+#   counter += 1
+# end
+
+# 5.times do 
+#   puts "Hello"
+# end
+def greeting
+  puts "Hello World"
+end
+greeting
---
+++ learning-if-end.rb
@@ -0,0 +1,14 @@
+=begin run_code_inside = false
+puts "Code before if...end"
+if run_code_inside
+  puts "code inside"
+end
+=end puts "Code after if...end"
+chance_of_rain = 1
+puts "Let's go outside!"
+if chance_of_rain > 0.5
+  puts "Pack an umbrella!"
+else
+  puts "Enjoy the fine day!"
+end
+puts "Oh, and always wear sunscreen!"
